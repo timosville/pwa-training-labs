14fe440d73aa39c66a913fd33a28477d06c25ce7
Timothy Koech <timosville@gmail.com>
2020-01-14 11:20:11 +0300

    Listen for life cylce events

---
+++ service-worker-lab/app/service-worker.js
@@ -0,0 +1,9 @@
+self.addEventListener("install", event => {
+  console.log("Service worker installing..");
+});
+
+self.addEventListener("activate", event => {
+  console.log("Service worker activating...");
+});
+
+self.skipWaiting();

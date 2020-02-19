cfe2531f61b49ebbbd030aec91af7d7fab8c206d
Timothy Koech <timosville@gmail.com>
2020-02-19 11:10:36 +0300

    Add Gulp

--- gulp-lab/project/gulpfile.js
+++ gulp-lab/project/gulpfile.js
@@ -1,9 +1,14 @@
 const gulp = require("gulp");
 const browserSync = require("browser-sync");
+const babel = require("gulp-babel");
 
 const copy = () => {
   return gulp
-    .src(["app/*.html", "app/**/*.jpg", "app/**/*.css", "app/**/*.js"])
+    .src([
+      "app/*.html",
+      "app/**/*.jpg",
+      "app/**/*.css" //"app/**/*.js"
+    ])
     .pipe(gulp.dest("build"));
 };
 
@@ -14,5 +19,14 @@ const serve = () => {
     port: 3000
   });
 };
+
+const processJs = () => {
+  return gulp
+    .src("app/scripts/*.js")
+    .pipe(babel({ presets: ["env"] }))
+    .pipe(gulp.dest("build/scripts"));
+};
+
+gulp.task("processJs", processJs);
 
 gulp.task("buildAndServe", gulp.series(copy, serve));

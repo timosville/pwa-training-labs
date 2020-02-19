8aed92a5fc49e518c5dbdfc23b46b9719e5cb224
Timothy Koech <timosville@gmail.com>
2020-02-19 09:37:30 +0300

    Inject a manifest into the service worker

--- workbox-lab/project/gulpfile.js
+++ workbox-lab/project/gulpfile.js
@@ -13,39 +13,67 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const gulp = require('gulp');
-const browserSync = require('browser-sync');
-const del = require('del');
+const gulp = require("gulp");
+const browserSync = require("browser-sync");
+const del = require("del");
+const workboxBuild = require("workbox-build");
+
+const buildSw = () => {
+  return workboxBuild
+    .injectManifest({
+      swSrc: "src/sw.js",
+      swDest: "build/sw.js",
+      globDirectory: "build",
+      globPatterns: [
+        "**/*.css",
+        "index.html",
+        "js/animation.js",
+        "images/home/*.jpg",
+        "images/icon/*.svg"
+      ]
+    })
+    .then(resources => {
+      console.log(
+        `Injected ${resources.count} resources for precaching, ` +
+          `totaling ${resources.size} bytes.`
+      );
+    })
+    .catch(err => {
+      console.log("Uh oh 😬", err);
+    });
+};
+
+gulp.task("build-sw", buildSw);
 
 // Clean "build" directory
 const clean = () => {
-  return del(['build/*'], {dot: true});
+  return del(["build/*"], { dot: true });
 };
-gulp.task('clean', clean);
+gulp.task("clean", clean);
 
 // Copy "src" directory to "build" directory
 const copy = () => {
- return gulp.src(['src/**/*']).pipe(gulp.dest('build'));
+  return gulp.src(["src/**/*"]).pipe(gulp.dest("build"));
 };
-gulp.task('copy', copy);
+gulp.task("copy", copy);
 
 // TODO - create a task to build the service worker
 
 // This is the app's build process
-const build = gulp.series('clean', 'copy');
-gulp.task('build', build);
+const build = gulp.series("clean", "copy", "build-sw");
+gulp.task("build", build);
 
 // Build the app, run a local dev server, and rebuild on "src" file changes
 const browserSyncOptions = {
-  server: 'build',
+  server: "build",
   port: 8081,
   open: false
 };
 const serve = gulp.series(build, () => {
   browserSync.init(browserSyncOptions);
-  return gulp.watch('src/**/*', build).on('change', browserSync.reload);
+  return gulp.watch("src/**/*", build).on("change", browserSync.reload);
 });
-gulp.task('serve', serve);
+gulp.task("serve", serve);
 
 // Set the default task to "build"
-gulp.task('default', build);
+gulp.task("default", build);

39828e70b2dfe5f9f65faf4c24ada89f213e5e5c
Timothy Koech <timosville@gmail.com>
2020-02-19 09:30:06 +0300

    Write basic sw

--- workbox-lab/project/src/sw.js
+++ workbox-lab/project/src/sw.js
@@ -1,1 +1,10 @@
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js"
+);
 
+if (workbox) {
+  console.log(`Workbox loaded`);
+  workbox.precaching.precacheAndRoute([]);
+} else {
+  console.log("Workbox didn't load");
+}

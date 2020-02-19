8e9ff487595f226e1cd7aed3bda32b9e9bf6dc6d
Timothy Koech <timosville@gmail.com>
2020-02-19 10:03:04 +0300

    Add routes to sw

--- workbox-lab/project/src/sw.js
+++ workbox-lab/project/src/sw.js
@@ -5,6 +5,19 @@ importScripts(
 if (workbox) {
   console.log(`Workbox loaded`);
   workbox.precaching.precacheAndRoute([]);
+
+  workbox.routing.registerRoute(
+    /(.*)articles(.*)\.(?:png|gif|jpg)/,
+    workbox.strategies.cacheFirst({
+      cacheName: "images-cache",
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 50,
+          maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+        })
+      ]
+    })
+  );
 } else {
   console.log("Workbox didn't load");
 }
